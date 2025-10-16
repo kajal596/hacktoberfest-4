742ea3dad520e91efbbee26dc7530ba2255034a4
kajal596 <kajalw9860@gmail.com>
2025-10-16 12:04:45 +0530

    Create fobinacci.java

---
+++ fobinacci.java
@@ -0,0 +1,15 @@
+public class JavatestClass
+{
+     public static void main(String []args)
+      {
+          int n=10,firstNum=0,secondNum=1;
+          int nextNum;
+        for(int i=1;i<=n;i++)
+        {
+          System.out.println(firstNum + " , ");
+          nextNum=firstNum+seconNum;
+          firstNum=secondNum;
+          secondNum=nextNum;
+        }
+    }
+}
